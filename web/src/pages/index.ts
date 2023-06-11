4f4fa284709ae54c24a5352db154ceae9b9901c0
Le0n <86106048+nevos08@users.noreply.github.com>
2023-06-11 17:02:09 +0200

    move hud to seperate resource

--- web/src/pages/index.ts
+++ web/src/pages/index.ts
@@ -2,6 +2,5 @@ import { FC } from 'react'
 
 import Multicharacter from './Multicharacter'
 import Creator from './Creator'
-import HUD from './HUD'
 
-export const PAGE_LIST: Record<string, FC> = { Multicharacter, Creator, HUD }
+export const PAGE_LIST: Record<string, FC> = { Multicharacter, Creator }
